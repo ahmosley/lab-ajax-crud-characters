7b9c641b94c5411f10c832c5b200fccc0fce4d40
Anna Mosley <annahmosley@gmail.com>
2021-03-07 10:12:39 -0500

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,44 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios
+    .get(this.BASE_URL + '/characters')
+    .then((responseFromApi) => {
+      const data = responseFromApi.data;
+    })
+    .catch((error) => console.log(`There was an error while getting all of the characters: ${error}`));
   }
 
   getOneRegister () {
+    axios
+    .get(this.BASE_URL + `/characters/${id}`)
+    .then((responseFromApi) => {
+      const data = responseFromApi.data;
+    })
+    .catch((error) => console.log(`There was an error while getting the character: ${error}`));
 
   }
 
   createOneRegister () {
+    axios
+    .post(this.BASE_URL + '/characters', newCharacter)
+    .then()
+    .catch((error) => console.log(`There was an error while creating the character: ${error}`));
 
   }
 
   updateOneRegister () {
-
+    axios
+    .put(this.BASE_URL + `/characters/${id}`)
+    .then()
+    .catch((error) => console.log(`There was an error while updating the character: ${error}`));
   }
 
   deleteOneRegister () {
+    axios
+    .delete(this.BASE_URL + `/characters/${id}`)
+    .then()
+    .catch((error) => console.log(`There was an error while deleting the character: ${error}`));
 
   }
 }
